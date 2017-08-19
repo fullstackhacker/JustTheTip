5ec1c9f980ef412f68456c92dd8307c186dcd4e4
kapadiamush <kapadiamush@gmail.com>
2017-08-19 01:36:02 -0700

    Initial commit for the Tip Calculator App.

---
+++ Just the Tip/SettingsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SettingsViewController.swift
+//  Just the Tip
+//
+//  Created by Mushaheed Kapadia on 8/18/17.
+//  Copyright © 2017 Mushaheed Kapadia. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBOutlet weak var defaultTipPercentageSlider: UISlider!
+    @IBOutlet weak var defaultTipValueLabel: UILabel!
+    
+    let storedTipKey = "default_tip"
+    let labelFormat = "%d%"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let defaults = UserDefaults.standard
+        let storedDefaultTip = defaults.integer(forKey: storedTipKey)
+        let sliderValue = storedDefaultTip * 2
+        defaultTipValueLabel.text = String.init(format: labelFormat, storedDefaultTip)
+        defaultTipPercentageSlider.value = Float(sliderValue) / 100
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func updateDefaultTipPercentage(_ sender: Any) {
+        let sliderValue = Int(defaultTipPercentageSlider.value * 100)
+        let defaultTip = sliderValue / 2
+        let defaults = UserDefaults.standard
+        defaults.set(defaultTip, forKey: storedTipKey)
+        defaults.synchronize()
+        
+        defaultTipValueLabel.text = String.init(format: labelFormat, defaultTip)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Just the Tip/ViewController.swift
+++ Just the Tip/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        calculateTip(self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let defaults = UserDefaults.standard
+        let tipPercentage = Double(defaults.integer(forKey: "default_tip")) / 100
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentage
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
