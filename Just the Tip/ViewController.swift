c4b658b811362b6143ecf2478a62e5a82f1bff26
kapadiamush <kapadiamush@gmail.com>
2017-09-02 16:05:34 -0400

    Implement a residual bill value if the app is restarted within 10 minutes.

--- Just the Tip/ViewController.swift
+++ Just the Tip/ViewController.swift
@@ -13,14 +13,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     
+    let residualBillValueKey = "residualBilValue"
+    let residualBillTimestampKey = "residualBillTimestamp"
+    let tenMinutesInSeconds = Double(600)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        // load residual bill timestamp
+        let currentTimestamp = NSDate()
+        
+        let defaults = UserDefaults.standard
+        let lastTimestamp = NSDate(timeIntervalSince1970: defaults.double(forKey: residualBillTimestampKey))
+        
+        let elapsedTime = currentTimestamp.timeIntervalSince(lastTimestamp as Date)
+        
+        if (elapsedTime < tenMinutesInSeconds) {
+            // hasn't been 10 minutes, load up residual bill value
+            let residualBillValue = defaults.double(forKey: residualBillValueKey)
+            billField.text = String(format: "%.2f", residualBillValue)
+        }
+        
         billField.becomeFirstResponder()
         calculateTip(self)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        let timestamp = Double(NSDate().timeIntervalSince1970)
+        let residualBillValue = Double(billField.text!) ?? 0
+        let defaults = UserDefaults.standard
+        defaults.set(timestamp, forKey: residualBillTimestampKey)
+        defaults.set(residualBillValue, forKey: residualBillValueKey)
     }
     
     override func didReceiveMemoryWarning() {
